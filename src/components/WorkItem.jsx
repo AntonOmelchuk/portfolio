56782ca88d4454d1ed1903384483f82e56a9ed60
Anton Omelchuk <kino_kg@ukr.net>
2019-10-25 18:26:33 +0300

    added works video

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export const WorkItem = ({img, link, title}) => {
+export const WorkItem = ({link, title, video}) => {
     return (
         <div className='works__item'>
-            <div className='work__image'>
-                <img className='work__screen' src={img}/>
+            <div className='work__video'>
+                <video src={video} autoPlay='autoPlay' loop className='work__screen' />
                 <div className='btn btn--work'><a href={link} target='_blank'>Watch</a></div>
             </div>
             <div className='work__title'>{title}</div>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {WorkItem} from "./WorkItem";
-import socialNetwork from '../images/works/socialNetwork.jpg';
-import todo from '../images/works/todo.jpg';
-import bookstore from '../images/works/bookstore.jpg';
-import musicArt from '../images/works/musicArt.jpg';
-import contactManger from '../images/works/contactManager.png';
+import socialNetwork from '../images/works/socialNetwork.mp4';
+import todo from '../images/works/todo.mp4';
+import bookstore from '../images/works/bookStore.mp4';
+import musicArt from '../images/works/musicArts.mp4';
+import contactManger from '../images/works/contactManager.mp4';
+import ticTacToe from '../images/works/ticTacToe.mp4';
 
 export const Works = () => {
 
@@ -12,34 +13,41 @@ export const Works = () => {
         {
             id: 1,
             title: 'Social Network',
-            img: socialNetwork,
+            video: socialNetwork,
             link: 'https://antonomelchuk.github.io/Social_Network/#/profile'
         },
         {
             id: 2,
             title: 'ToDo List',
-            img: todo,
+            video: todo,
             link: 'https://reacttodosua.herokuapp.com'
         },
         {
             id: 3,
             title: 'Bookstore',
-            img: bookstore,
+            video: bookstore,
             link: 'https://react-bookstore-ua.herokuapp.com'
         },
         {
             id: 4,
             title: 'MusicArts',
-            img: musicArt,
+            video: musicArt,
             link: 'https://antonomelchuk.github.io/MusicArts/index.html'
         },
         {
             id: 5,
             title: 'ContactManager',
-            img: contactManger,
+            video: contactManger,
             link: 'https://contact-manager-ua.herokuapp.com/'
+        },
+        {
+            id: 6,
+            title: 'Tic Tac Toe',
+            video: ticTacToe,
+            link: 'https://antonomelchuk.github.io/Tic_Tac_Toe/'
         }
     ];
+
 
     return (
         <div className='works block'>
@@ -47,7 +55,8 @@ export const Works = () => {
                 <div className='block-title'>My works</div>
                 <div className='works__inner'>
                     {works.map(work => {
-                        return <WorkItem key={work.id} title={work.title} img={work.img} link={work.link} />
+                        return <WorkItem key={work.id} title={work.title} img={work.img} link={work.link}
+                                         video={work.video}/>
                     })}
                 </div>
             </div>
