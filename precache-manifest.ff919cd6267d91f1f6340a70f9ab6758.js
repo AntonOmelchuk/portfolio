73e58335ae13c85982e855d44923d85b9b34c58a
Anton Omelchuk <kino_kg@ukr.net>
2019-11-02 21:46:46 +0200

    Updates

---
+++ precache-manifest.ff919cd6267d91f1f6340a70f9ab6758.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c266ef2fa1c6013cdaf614aeee3054fc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "22c20d061d0d5288f556",
+    "url": "/portfolio/static/css/main.973989a5.chunk.css"
+  },
+  {
+    "revision": "9c197278035948c5e671",
+    "url": "/portfolio/static/js/2.e1ffa689.chunk.js"
+  },
+  {
+    "revision": "22c20d061d0d5288f556",
+    "url": "/portfolio/static/js/main.6818b97e.chunk.js"
+  },
+  {
+    "revision": "5029282666060c428c88",
+    "url": "/portfolio/static/js/runtime~main.cbff171f.js"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/React.47ce6e77.png"
+  },
+  {
+    "revision": "975e58234d59ca61f1e1f37d3b29d6c6",
+    "url": "/portfolio/static/media/bookStore.975e5823.mp4"
+  },
+  {
+    "revision": "a45bc740c15ff999d62b859da0880396",
+    "url": "/portfolio/static/media/contactManager.a45bc740.mp4"
+  },
+  {
+    "revision": "9fe7584b6d29bec74a66024829929697",
+    "url": "/portfolio/static/media/css.9fe7584b.png"
+  },
+  {
+    "revision": "dc689da310e77bdd4eedb887dc2e15bf",
+    "url": "/portfolio/static/media/gitfinder.dc689da3.mp4"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/portfolio/static/media/html5.2ea01c0e.png"
+  },
+  {
+    "revision": "300c11bd0b5bfbae85c55c99b283766a",
+    "url": "/portfolio/static/media/musicArts.300c11bd.mp4"
+  },
+  {
+    "revision": "75440f122b227012e847648afefccc72",
+    "url": "/portfolio/static/media/photo.75440f12.jpg"
+  },
+  {
+    "revision": "62cc7d7a7701fc5c78e8bfdfe1c58ad7",
+    "url": "/portfolio/static/media/redux.62cc7d7a.png"
+  },
+  {
+    "revision": "0486d868001084c9ddc60f3187c0343e",
+    "url": "/portfolio/static/media/socialNetwork.0486d868.mp4"
+  },
+  {
+    "revision": "4738a20e4770c360aa09e7bb0b4ff9dc",
+    "url": "/portfolio/static/media/ticTacToe.4738a20e.mp4"
+  },
+  {
+    "revision": "bb2627da653eae6af74f66995026f37b",
+    "url": "/portfolio/static/media/todo.bb2627da.mp4"
+  }
+]);
