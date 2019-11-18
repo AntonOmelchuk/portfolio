c66c427debb2bc3966b901c0c4016cc2fde57b8d
Anton Omelchuk <kino_kg@ukr.net>
2019-11-18 20:27:33 +0200

    add eslint

--- src/components/SkilItem.jsx
+++ src/components/SkilItem.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 
-export const SkillItem = ({icon, title, description}) => {
-    return (
-        <div className='skills__item'>
-            <div className='skills__icon'>
-                <img src={icon} />
-            </div>
-            <div className='skills__title'>{title}</div>
-            <div className='skills__description'>
-                {description}
-            </div>
-        </div>
-    )
+export const SkillItem = ({ icon, title, description }) => {
+  return (
+    <div className="skills__item">
+      <div className="skills__icon">
+        <img src={icon} alt="skill icon" />
+      </div>
+      <div className="skills__title">{title}</div>
+      <div className="skills__description">{description}</div>
+    </div>
+  );
 };
-
